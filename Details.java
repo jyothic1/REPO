ff96649ffe05b530bfed5c0b11b458018ea1ae19
unknown <jyothic.xworkz@gmail.com>
2022-09-15 08:52:24 +0530

    uploading

---
+++ Details.java
@@ -0,0 +1,46 @@
+class Details{
+	static String getNameByMobile(long mobile)
+	{
+		if(mobile==9100703797l)
+		{
+		System.out.println("mobile no is : "+mobile);
+		return "jyothi";
+			
+		}
+		else if(mobile==9986667354l)
+		{
+				
+		System.out.println("mobile no is : "+mobile);
+		return "OM";
+		}
+		else if(mobile==9901977065l)
+		{
+			
+			System.out.println("mobile no is : "+mobile);
+          return "sanjay";			
+		}
+		else if(mobile==9986812821l)
+		{
+        System.out.println("mobile no is : "+mobile);
+		return "vinutha";
+		}
+		else if(mobile==7676688637l)
+		{
+		 System.out.println("mobile no is : "+mobile);
+    return "vikram";	 
+		}
+		else if(mobile==7760503597l){
+					 System.out.println("mobile no is : "+mobile);
+					 return "ashwini";
+					 
+			
+		}
+		return null;
+		
+		
+	}
+	
+	
+	
+	
+}
---
+++ DetailsRunner.java
@@ -0,0 +1,13 @@
+class DetailsRunner{
+	public static void main(String[]number)
+	{
+		
+		long no=91007037l;
+		String name=Details.getNameByMobile(no);
+		System.out.println(name);
+		
+		
+	}
+	}
+
+
